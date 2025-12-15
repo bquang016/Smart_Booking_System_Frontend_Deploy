9aadc74ee10beec153ea008cd7bd119a370b855f
bquang016 <buidangquangmtp@gmail.com>
2025-12-16 03:37:53 +0700

    xong

--- src/services/axios.config.js
+++ src/services/axios.config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:8386/api/v1";
+const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8386/api/v1";
 
 const api = axios.create({
     baseURL: API_URL,
